830a06ad3778650a527b5c423e1b1d849cf69b6f
Pol Maresch <polmaresch@gmail.com>
2024-01-27 17:36:04 +0100

    Spawner casi acabat

--- Assets/Scripts/Enemy/EnemyGenerator.cs
+++ Assets/Scripts/Enemy/EnemyGenerator.cs
@@ -5,53 +5,85 @@ using UnityEngine.AI;
 public class EnemyGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject[] enemies;
-    private int numEnemies;
+    public GameObject[] enemiesType;
     public Unity.AI.Navigation.NavMeshSurface naveMeshLayer;
+    List<GameObject> pullEnemies = new List<GameObject>();
+    private float lastGenerated = 0;
+    public float generationrate;
     void Start()
     {
-        StartCoroutine(EnemySpawner());
+        CreatePool();
     }
 
     // Update is called once per frame
     void Update()
     {
-            
+        lastGenerated += Time.deltaTime;
+        if (lastGenerated >= generationrate)
+        {
+            Spawner();
+            lastGenerated = 0f;
+        }
+
     }
 
-    IEnumerator EnemySpawner()
-    {
-        float posX = 0;
-        float posZ = 0;
-        while (numEnemies < 15)
+    void Spawner()
+    {   
+        float posX = 0f;
+        float posZ = 0f;
+        GameObject newEnemy = pullEnemies[enemyRandomizer(pullEnemies.Count)];
+        Vector3 randomPosition = setRandomPosition(posX, posZ);
+        if (CheckInNaveMesh(randomPosition) == true && newEnemy.activeSelf == false)
         {
-            posX = Random.Range(gameObject.transform.position.x - 10f, gameObject.transform.position.x + 10f);
-            posZ = Random.Range(gameObject.transform.position.z - 10f, gameObject.transform.position.z + 10f);
-           // if (CheckInNaveMesh(new Vector3(posX, enemies[enemyRandomizer()].transform.position.y, posZ)) == true)
-            {
-                Instantiate(enemies[enemyRandomizer()], new Vector3(posX, enemies[enemyRandomizer()].transform.position.y, posZ), Quaternion.identity);
-                yield return new WaitForSeconds(3f);
-                numEnemies += 1;
-            }
-
+            newEnemy.transform.position = randomPosition;
+            newEnemy.SetActive(true);
         }
     }
-
-    int enemyRandomizer()
+    int enemyRandomizer(int maxrange)
     {
         int randomEnemy;
-        randomEnemy = Random.Range(0, 3);
+        randomEnemy = Random.Range(0, maxrange);
         return randomEnemy;
     }
 
-   // bool CheckInNaveMesh(Vector3 randPosition)
-   // {
-   //     NavMeshHit hit;
-   //     if (NavMesh.SamplePosition(randPosition, out hit, Mathf.Infinity, NavMesh.GetAreaFromName("Walkable")))
-   //     {
-   //         return true;
-   //     }
-   //     return false;
-   // }
+   bool CheckInNaveMesh(Vector3 randPosition)
+   {
 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randPosition, out hit, 1.0f, NavMesh.GetAreaFromName("NavMesh_Terrain")))
+        {
+            return true;
+        }
+       return false;
+   }
+
+    Vector3 setRandomPosition(float posX, float posZ)
+    {
+        posX = Random.Range(gameObject.transform.position.x - 10f, gameObject.transform.position.x + 10f);
+        posZ = Random.Range(gameObject.transform.position.z - 10f, gameObject.transform.position.z + 10f);
+        Vector3 randomPos = new Vector3(posX, 0.6f, posZ);
+        return randomPos;
+    }
+
+    void CreatePool()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject newEnemy = Instantiate(enemiesType[0], new Vector3(0, 100, 0), Quaternion.identity, transform);
+            newEnemy.SetActive(false);
+            pullEnemies.Add(newEnemy);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject newEnemy = Instantiate(enemiesType[1], new Vector3(0, 100, 0), Quaternion.identity, transform);
+            newEnemy.SetActive(false);
+            pullEnemies.Add(newEnemy);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject newEnemy = Instantiate(enemiesType[2], new Vector3(0, 100, 0), Quaternion.identity, transform);
+            newEnemy.SetActive(false);
+            pullEnemies.Add(newEnemy);
+        }
+    }
 }
