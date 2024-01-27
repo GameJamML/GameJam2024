ab36b5f77c1162f922c0972320c4cbe28278250c
Pol Maresch <polmaresch@gmail.com>
2024-01-28 00:37:34 +0100

    Text Heal Baby

---
+++ Assets/Scripts/HealBaby.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealBaby : MonoBehaviour
+{
+    public GameObject healText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healText.gameObject.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            healText.gameObject.SetActive(true);
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            healText.gameObject.SetActive(false);
+        }
+    }
+
+}
