6eb4888d7cc6b43bf56fef9a2914b89df7fe2045
MrIllas <44169046+MrIllas@users.noreply.github.com>
2024-01-28 17:21:07 +0100

    Moebius finished

--- Assets/Shader/Custom Lighting/CustomLighting.hlsl
+++ Assets/Shader/Custom Lighting/CustomLighting.hlsl
@@ -25,30 +25,6 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
 #endif
 }
 
-void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
-{
-#if SHADERGRAPH_PREVIEW
-    Direction = half3(0.5, 0.5, 0);
-    Color = 1;
-    DistanceAtten = 1;
-    ShadowAtten = 1;
-#else
-#if SHADOWS_SCREEN
-    half4 clipPos = TransformWorldToHClip(WorldPos);
-    half4 shadowCoord = ComputeScreenPos(clipPos);
-#else
-    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-#endif
-    Light mainLight = GetMainLight(shadowCoord);
-    Direction = mainLight.direction;
-    Color = mainLight.color;
-    DistanceAtten = mainLight.distanceAttenuation;
-    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
-    float shadowStrength = GetMainLightShadowStrength();
-    ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowStrength, false);
-#endif
-}
-
 void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, float3 Color, float3 WorldNormal, float3 WorldView, out float3 Out)
 {
 #if SHADERGRAPH_PREVIEW
@@ -61,62 +37,39 @@ void DirectSpecular_float(float3 Specular, float Smoothness, float3 Direction, f
 #endif
 }
 
-void DirectSpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out)
-{
-#if SHADERGRAPH_PREVIEW
-    Out = 0;
-#else
-    Smoothness = exp2(10 * Smoothness + 1);
-    WorldNormal = normalize(WorldNormal);
-    WorldView = SafeNormalize(WorldView);
-    Out = LightingSpecular(Color, Direction, WorldNormal, WorldView, half4(Specular, 0), Smoothness);
-#endif
-}
-
 void AdditionalLights_float(float3 SpecColor, float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, out float3 Diffuse, out float3 Specular)
 {
-    float3 diffuseColor = 0;
-    float3 specularColor = 0;
+    float3 diffuseColor = 1;  // Initialize to zero
+    float3 specularColor = 1; // Initialize to zero
 
 #ifndef SHADERGRAPH_PREVIEW
     Smoothness = exp2(10 * Smoothness + 1);
     WorldNormal = normalize(WorldNormal);
     WorldView = SafeNormalize(WorldView);
     int pixelLightCount = GetAdditionalLightsCount();
+
+    int numSteps = 1; // Set to the number of desired steps
+
     for (int i = 0; i < pixelLightCount; ++i)
     {
         Light light = GetAdditionalLight(i, WorldPosition);
         half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
-        diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
-        specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, float4(SpecColor, 0), Smoothness);
+
+        // Calculate Lambertian and Specular contributions
+        float lambertian = LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+        float specular = LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, float4(SpecColor, 0), Smoothness);
+
+        // Apply step function to the contributions
+        lambertian = round(lambertian * (numSteps)) / (numSteps);
+        specular = round(specular * (numSteps)) / (numSteps);
+
+        // Accumulate contributions
+        diffuseColor += lambertian * attenuatedLightColor; // Multiply by attenuatedLightColor
+        specularColor += specular * attenuatedLightColor; // Multiply by attenuatedLightColor
     }
 #endif
 
     Diffuse = diffuseColor;
     Specular = specularColor;
 }
-
-void AdditionalLights_half(half3 SpecColor, half Smoothness, half3 WorldPosition, half3 WorldNormal, half3 WorldView, out half3 Diffuse, out half3 Specular)
-{
-    half3 diffuseColor = 0;
-    half3 specularColor = 0;
-
-#ifndef SHADERGRAPH_PREVIEW
-    Smoothness = exp2(10 * Smoothness + 1);
-    WorldNormal = normalize(WorldNormal);
-    WorldView = SafeNormalize(WorldView);
-    int pixelLightCount = GetAdditionalLightsCount();
-    for (int i = 0; i < pixelLightCount; ++i)
-    {
-        Light light = GetAdditionalLight(i, WorldPosition);
-        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
-        diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
-        specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, half4(SpecColor, 0), Smoothness);
-    }
-#endif
-
-    Diffuse = diffuseColor;
-    Specular = specularColor;
-}
-
 #endif
