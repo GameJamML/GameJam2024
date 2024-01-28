b9e5349b978a1dabb74da0bcf761bf7611de1449
Yogui <972081300@qq.com>
2024-01-28 15:05:11 +0100

    Attack effect

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField, Range(0.01f, 0.3f)] private float _attakDelaySpeed;
+    [SerializeField] private Transform _attackTransform;
     [SerializeField] private Transform _mirrorTransform;
     [SerializeField] private GameObject _attackRange;
+    [SerializeField] private ParticleSystem _attackParticle;
 
     private Animator _anim;
 
@@ -47,19 +49,25 @@ public class PlayerAttack : MonoBehaviour
     {
         for (float t = 0; t < 1; t += _attakDelaySpeed)
         {
-            _mirrorTransform.localScale = Vector3.Lerp(Vector3.one * 0.3f, Vector3.one, t);
+            _attackTransform.localScale = Vector3.Lerp(Vector3.one * 0.3f, Vector3.one, t);
 
             yield return null;
         }
+
+        _attackParticle.transform.parent.transform.position = _mirrorTransform.position;
+
+        _attackParticle.Play();
     }
 
     private IEnumerator AttackEnding()
     {
         for (float t = 0; t < 1; t += _attakDelaySpeed)
         {
-            _mirrorTransform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * 0.3f, t);
+            _attackTransform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * 0.3f, t);
 
             yield return null;
         }
+
+        _attackParticle.Stop();
     }
 }
