561458078b74ef07a7f2ca03c4c36c7141acae34
Yogui <972081300@qq.com>
2024-01-29 15:02:59 +0100

    Optimize catch process

--- Assets/Scripts/UI/EnemyCatch.cs
+++ Assets/Scripts/UI/EnemyCatch.cs
@@ -1,36 +1,88 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyCatch : MonoBehaviour
 {
-    private List<KeyCode> _catchKeyList = new();
-    [SerializeField]private Image[] _keyImages;
-    private bool _startCatching;
-    private int _catchIndex = 0;
+    private class KeyToPress
+    {
+        public KeyToPress(Image img)
+        {
+            image = img;
+        }
+
+        public void AddKey(KeyCode key)
+        {
+            switch (key)
+            {
+                case KeyCode.UpArrow:
+                    alternativeKey = KeyCode.W;
+                    break;
+                case KeyCode.DownArrow:
+                    alternativeKey = KeyCode.S;
+                    break;
+                case KeyCode.LeftArrow:
+                    alternativeKey = KeyCode.A;
+                    break;
+                case KeyCode.RightArrow:
+                    alternativeKey = KeyCode.D;
+                    break;
+                default:
+                    return;
+            }
+
+            this.key = key;
+        }
+
+        public KeyCode key = KeyCode.None;
+        public KeyCode alternativeKey;
+        public Image image;
+    }
+
+    // Resources of sprites
+    [SerializeField]
+    private List<KeySpritePair> _spriteList;
+    [Serializable]
+    private class KeySpritePair
+    {
+        public KeyCode key;
+        public Sprite value;
+    }
+
+    private List<KeyToPress> _keyToPressList = new();
+
+    [SerializeField, Header("Only for read")] private bool _startCatching = false;
+    [SerializeField, Header("Only for read")] private int _catchIndex = 1;
     private int _difficult = 4;
 
     public Action<bool> endToCatch;
 
+    private RectTransform _myRecTransform;
+
     private void Start()
     {
-        _keyImages = GetComponentsInChildren<Image>();
+        _myRecTransform = GetComponent<RectTransform>();
+        Image[] _keyImages = GetComponentsInChildren<Image>();
 
-        foreach (var item in _keyImages)
+        foreach (var img in _keyImages)
         {
-            item.gameObject.SetActive(false);
+            img.gameObject.SetActive(false);
+
+            KeyToPress kp = new(img);
+
+            _keyToPressList.Add(kp);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        TryToCatch();
-
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Space))
             StartToCatch();
+        else
+            TryToCatch();
     }
 
     public void StartToCatch()
@@ -38,24 +90,47 @@ public class EnemyCatch : MonoBehaviour
         if (_startCatching)
             return;
 
-        _startCatching = true;
-
-        _catchIndex = 0;
-
-        Dictionary<KeyCode, Color> keyPool = new()
+        if (_catchIndex != 0)
         {
-            { KeyCode.A, Color.red },
-            { KeyCode.S, Color.green },
-            { KeyCode.D, Color.blue },
-            { KeyCode.W, Color.white },
-        };
+            _catchIndex = 0;
 
-        for (int i = 0; i < _difficult; i++)
-        {
-            _catchKeyList.Add(keyPool.OrderBy(x => UnityEngine.Random.value).First().Key);
-            _keyImages[i].gameObject.SetActive(true);
-            _keyImages[i].color = keyPool[_catchKeyList[i]];
+            // Init keys
+            for (int i = 0; i < _difficult; i++)
+            {
+                KeySpritePair kp = _spriteList[UnityEngine.Random.Range(0, _spriteList.Count)];
+
+                _keyToPressList[i].AddKey(kp.key);
+                _keyToPressList[i].image.sprite = kp.value;
+                _keyToPressList[i].image.gameObject.SetActive(true);
+            }
+
+            StartCoroutine(StartToCatchAnim());
         }
+    }
+
+    private IEnumerator StartToCatchAnim()
+    {
+        Vector2 beginPos = new(0, -30);
+        Vector2 endPos = new(0, 0);
+
+        Color beginColor = Color.white;
+        Color endColor = Color.white;
+
+        beginColor.a = 0;
+
+        for (float t = 0; t < 1; t += Time.deltaTime * 2)
+        {
+            for (int i = 0; i < _difficult; i++)
+            {
+                _keyToPressList[i].image.color = Color.Lerp(beginColor, endColor, t);
+            }
+
+            _myRecTransform.localPosition = Vector2.Lerp(beginPos, endPos, t);
+
+            yield return null;
+        }
+
+        _startCatching = true;
     }
 
     private void TryToCatch()
@@ -67,26 +142,43 @@ public class EnemyCatch : MonoBehaviour
             return;
 
         // if press wrong key
-        if (!Input.GetKeyDown(_catchKeyList[_catchIndex]))
+        if (!Input.GetKeyDown(_keyToPressList[_catchIndex].key) && !Input.GetKeyDown(_keyToPressList[_catchIndex].alternativeKey))
         {
-            _catchKeyList.Clear();
-            _startCatching = false;
-            // catch fail
-            //endToCatch.Invoke(false);
+            CatchFaild();
         }
         else
         {
-            _keyImages[_catchIndex++].gameObject.SetActive(false);
+            _keyToPressList[_catchIndex++].image.gameObject.SetActive(false);
 
+            // when finishd catch
             if (_catchIndex >= _difficult)
             {
-                _catchIndex = 0;
                 _startCatching = false;
-                _catchKeyList.Clear();
+                _catchIndex = -1;
+
+                // Close All images
+                for (int i = 0; i < _difficult; i++)
+                {
+                    _keyToPressList[i].image.gameObject.SetActive(false);
+                }
                 // catch succesful
                 //endToCatch.Invoke(true);
             }
         }
 
+    }
+
+    private void CatchFaild()
+    {
+        for (int i = 0; i < _difficult; i++)
+        {
+            _keyToPressList[i].image.gameObject.SetActive(false);
+        }
+
+        _startCatching = false;
+        _catchIndex = -1;
+
+        // catch fail
+        //endToCatch.Invoke(false);
     }
 }
